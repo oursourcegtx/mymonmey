d55398443f436e272b7bee18011407c0cc996a27
oursourcegtx <93318626+oursourcegtx@users.noreply.github.com>
2022-02-12 16:04:27 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,7 +1,5 @@
 cd /home/azure
-woker=$(date +'%d%m_%H-')
-myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-woker+=$myip
+woker+=$(date +'%d%m_%H%M%S')
 if [[ ! -f isHaveSetupCoin.txt ]]
 then
     echo "Start setup..."
